1e99f7c7d29d0fb9c9cf69c78dd55f93525c34e9
Maryam <tamarahaak@web.de>
2024-07-20 08:30:50 +0200

    finish project

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,30 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-
-
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.scheduledFunction = functions.pubsub.schedule('every 24 hours').onRun(async (context) => {
-  const db = admin.firestore();
-  const usersRef = db.collection('users');
-  
-  // Get top 10 users
-  const topUsersSnapshot = await usersRef.orderBy('averageRating', 'desc').limit(10).get();
-  const topUsersIds = topUsersSnapshot.docs.map(doc => doc.id);
+exports.scheduledFunction = functions.pubsub.schedule("every 24 hours")
+    .onRun(async (context) => {
+      const db = admin.firestore();
+      const usersRef = db.collection("users");
 
-  // Get all users
-  const allUsersSnapshot = await usersRef.get();
-  const allUsersIds = allUsersSnapshot.docs.map(doc => doc.id);
+      // Get top 10 users
+      const topUsersSnapshot = await usersRef.orderBy("averageRating", "desc")
+          .limit(10).get();
+      const topUsersIds = topUsersSnapshot.docs.map((doc) => doc.id);
 
-  // Filter out top users
-  const usersToDelete = allUsersIds.filter(id => !topUsersIds.includes(id));
+      // Get all users
+      const allUsersSnapshot = await usersRef.get();
+      const allUsersIds = allUsersSnapshot.docs.map((doc) => doc.id);
 
-  // Delete users
-  const batch = db.batch();
-  usersToDelete.forEach(userId => {
-    batch.delete(usersRef.doc(userId));
-  });
+      // Filter out top users
+      const usersToDelete = allUsersIds.filter((id)=>!topUsersIds.includes(id));
 
-  await batch.commit();
-  console.log('Deleted users except top 10');
-});
+      // Delete users
+      const batch = db.batch();
+      usersToDelete.forEach((userId) => {
+        batch.delete(usersRef.doc(userId));
+      });
+
+      await batch.commit();
+      console.log("Deleted users except top 10");
+    });
